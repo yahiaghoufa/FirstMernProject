4dd357f049223b3c7357bb23ed7f413886eb4a84
Yahia Ghoufa <yahiaghoufa910@gmail.com>
2025-06-25 18:37:16 +0100

    final

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,30 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import path from 'path';
-import { connectDB } from './config/db.js';
-import productRoutes from './routes/product.route.js';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.use('/api/products', productRoutes);
+app.use("/api/products", productRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-  );
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
-connectDB()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server started at http://localhost:${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Failed to connect to DB:', error);
-    process.exit(1);
-  });
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
+});
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+			},
+		},
+	},
 });
